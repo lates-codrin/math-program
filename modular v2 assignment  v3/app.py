6034d1d0035ca83f7add962d648db3ede4d50dba
lates-codrin <latescodrin@gmail.com>
2024-10-21 16:20:21 +0300

    Delete modular v2 assignment  v3 directory
    Wrong upload

--- modular v2 assignment  v3/app.py
+++
@@ -1,3 +0,0 @@
-from ui.console import run
-
-run()
--- modular v2 assignment  v3/features/list_undo.py
+++
@@ -1,7 +0,0 @@
-def undo(v: list, last: list)->list:
-    v.clear()
-    v.extend(last[-1])
-    del(last[-1])
-    return v
-
-# work in progress
